c07a81371ca8985010b9d7dba356029ccf3fed64
Sami Kilpinen <sami.kilpinen@helsinki.fi>
2023-10-09 12:22:40 +0300

    Save to qs

--- ReadTobiasSnakemake.R
+++ ReadTobiasSnakemake.R
@@ -5,4 +5,6 @@ library(tidyverse)
 source_url("https://github.com/ComputationalNeurogenetics/AuxCode/raw/master/AuxFunctions.R")
 
 # Read TOBIAS results from snakemake output
-rV2.groups.tobias <- get_BINDetect_snakemake_results_v2("/Volumes/LaCie/E12rV2_groups_200923/TFBS/", parallel = TRUE, mc.cores = 4)
+rV2.groups.tobias.gr <- get_BINDetect_snakemake_results_gr("/Volumes/LaCie/E12rV2_groups_200923/TFBS/", parallel = TRUE, mc.cores = 4)
+
+qsave(rV2.groups.tobias.gr, file = "analysis/rV2.groups.tobias.gr.qs", nthreads = 6)
