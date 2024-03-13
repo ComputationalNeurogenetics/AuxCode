8b2cf5e4fa090c1057b9b55e4134e966380226e1
Kilpinen <kilpinen@dm3-576-29475.pc.helsinki.fi>
2024-03-13 11:07:52 +0200

    typo fix

--- TOBIAS_tools.R
+++ TOBIAS_tools.R
@@ -806,7 +806,7 @@ extract.factors <- function(db.name, gene_name, group_name, zscore.thr=0, full.d
   if(!any(group_name %in% c("PRO1_2","CO1_2","GA1_2","GA3_4","GA5_6","GL1_2","GL3_4","GL5"))){stop("group_name must be one of the following: PRO1_2, CO1_2, GA1_2, GA3_4, GA5_6, GL1_2, GL3_4, GL5")}
   
   if (!is.null(zscore.thr)){
-    query<-paste('SELECT tb.* FROM tobias as tb, exp as exp, acc as ac, links as links, gene_metadata as gm WHERE tb.features==ac.features AND (qc.',group_name,'>0.61) AND tb.features==links.feature AND tb.mean_cons>0.5 AND links.zscore>0 AND exp.ensg_id=tb.ensg_id AND (tb.',group_name,'_bound=1) AND (exp.',group_name,'>1.2) AND links.ensg_id=gm.ensg_id AND gm.gene_name="',gene_name,'" AND links.zscore>',zscore.thr,'', sep="")
+    query<-paste('SELECT tb.* FROM tobias as tb, exp as exp, acc as ac, links as links, gene_metadata as gm WHERE tb.features==ac.features AND (ac.',group_name,'>0.61) AND tb.features==links.feature AND tb.mean_cons>0.5 AND links.zscore>0 AND exp.ensg_id=tb.ensg_id AND (tb.',group_name,'_bound=1) AND (exp.',group_name,'>1.2) AND links.ensg_id=gm.ensg_id AND gm.gene_name="',gene_name,'" AND links.zscore>',zscore.thr,'', sep="")
   } else {
     print("zscore.thr is NULL, ignoring gene_name and fetching all bound TF events for the group and exp condition")
     query<-paste('SELECT tb.* FROM tobias as tb, exp as exp, gene_metadata as gm WHERE tb.mean_cons>0.5 AND exp.ensg_id=tb.ensg_id AND (tb.',group_name,'_bound=1) AND (exp.',group_name,'>1.2) AND gm.gene_name="',gene_name,'"', sep="")
